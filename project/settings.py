53670be52e3239d521d32f25d0094d54c7ce7e0f
E'zoza <ezozaxolmamatova905@gmail.com>
2024-10-05 19:26:08 +0500

    email

--- project/settings.py
+++ project/settings.py
@@ -129,3 +129,11 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'askarjon.abdullayev'
+EMAIL_HOST_PASSWORD = 'kdrerwbklpoooxlp'
